8c7a302d3e331922f9f3a772f50421e7b2589418
NiloyMallick <mallickniloy@gmail.com>
2022-03-12 22:08:48 +0600

    static root add

--- JobBuilder/settings.py
+++ JobBuilder/settings.py
@@ -121,6 +121,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'staticfiles'
+
+STATICFILES_DIRS = [BASE_DIR/'statics']
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+# Media configs
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
